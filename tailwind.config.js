2ca00a13a7f89354f7b8946df6473c80a0827a7e
Paul <pdevito3@gmail.com>
2020-02-29 15:22:17 -0500

    tailwindui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/ui')({
+      layout: 'sidebar',
+    })
+  ]
 }
