4bd741666c6772417989f7cdce2e42d3277d123f
Paul <pdevito3@gmail.com>
2020-02-29 10:57:03 -0500

    tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app" class="text-purple-500 font-bold text-3xl">
+    Hello Vue
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>-->
+  </div>
+</template>
+
+<style>
+</style>
