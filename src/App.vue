11e94b6451b5796536099c2b5daf365819fcbe89
Paul <pdevito3@gmail.com>
2020-03-03 00:02:37 -0500

    started abstracting sidebar searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,190 +1,9 @@
 <template>
   <!-- @keydown.window.escape="sidebarOpen = false" -->
   <div class="h-screen flex overflow-hidden bg-gray-100" x-data="{ sidebarOpen: false }">
-    <!-- Off-canvas menu for mobile -->
-    <div class="md:hidden">
-      <div
-        @click="sidebarOpen = false"
-        class="fixed inset-0 z-30 bg-gray-600 opacity-0 pointer-events-none transition-opacity ease-linear duration-300"
-        :class="{'opacity-75 pointer-events-auto': sidebarOpen, 'opacity-0 pointer-events-none': !sidebarOpen}"
-      ></div>
-
-      <!-- 
-        this was on class, but I needed to take it off for it to work
-      -translate-x-full-->
-      <div
-        class="fixed inset-y-0 left-0 flex flex-col z-40 max-w-xs w-full pt-5 pb-4 bg-red-800 transform ease-in-out duration-300"
-        :class="{'translate-x-0': sidebarOpen, '-translate-x-full': !sidebarOpen}"
-      >
-        <div class="absolute top-0 right-0 -mr-14 p-1">
-          <button
-            v-show="sidebarOpen"
-            @click="sidebarOpen = false"
-            class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
-          >
-            <svg class="h-6 w-6 text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>
-          </button>
-        </div>
-        <div class="flex-shrink-0 flex items-center px-4">
-          <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-brand.svg" alt="Workflow" />
-        </div>
-        <div class="mt-5 flex-1 h-0 overflow-y-auto">
-          <nav class="px-2">
-            <a
-              href="#"
-              class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-red-900 focus:outline-none focus:bg-red-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-4 h-6 w-6 text-red-400 group-hover:text-red-300 group-focus:text-red-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
-                />
-              </svg>
-              Dashboard
-            </a>
-          </nav>
-        </div>
-      </div>
-    </div>
-
-    <!-- Static sidebar for desktop -->
-    <div class="hidden md:flex md:flex-shrink-0">
-      <div class="flex flex-col w-64 bg-red-800 pt-5 pb-4">
-        <div class="flex items-center flex-shrink-0 px-4">
-          <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-brand.svg" alt="Workflow" />
-        </div>
-        <div class="mt-5 h-0 flex-1 flex flex-col overflow-y-auto">
-          <!-- Sidebar component, swap this element with another sidebar if you like -->
-          <nav class="flex-1 px-2 bg-red-800">
-            <a
-              href="#"
-              class="group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md bg-red-900 focus:outline-none focus:bg-red-700 transition ease-in-out duration-150"
-            >
-              <svg
-                class="mr-3 h-6 w-6 text-red-400 group-focus:text-red-300 transition ease-in-out duration-150"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
-                />
-              </svg>
-              Dashboard
-            </a>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <sidebar :sidebarOpen="sidebarOpen" @toggleSidebar="toggleSidebar" />
     <div class="flex flex-col w-0 flex-1 overflow-hidden">
-      <div class="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
-        <button
-          @click.stop="sidebarOpen = true"
-          class="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:bg-gray-100 focus:text-gray-600 md:hidden"
-        >
-          <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h7"
-            />
-          </svg>
-        </button>
-        <div class="flex-1 px-4 flex justify-between">
-          <div class="flex-1 flex">
-            <div class="w-full flex md:ml-0">
-              <label for="search_field" class="sr-only">Search</label>
-              <div class="relative w-full text-gray-400 focus-within:text-gray-600">
-                <div class="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                  <svg class="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  id="search_field"
-                  class="block w-full h-full pl-8 pr-3 py-2 rounded-md text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 sm:text-sm"
-                  placeholder="Search"
-                />
-              </div>
-            </div>
-          </div>
-          <div class="ml-4 flex items-center md:ml-6">
-            <button
-              class="p-1 text-gray-400 rounded-full hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:shadow-outline focus:text-gray-500"
-            >
-              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-            </button>
-            <div @click.away="open = false" class="ml-3 relative" x-data="{ open: false }">
-              <div>
-                <button
-                  @click="open = !open"
-                  class="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:shadow-outline"
-                >
-                  <img
-                    class="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt
-                  />
-                </button>
-              </div>
-              <div
-                v-show="open"
-                x-transition:enter="transition ease-out duration-100"
-                x-transition:enter-start="transform opacity-0 scale-95"
-                x-transition:enter-end="transform opacity-100 scale-100"
-                x-transition:leave="transition ease-in duration-75"
-                x-transition:leave-start="transform opacity-100 scale-100"
-                x-transition:leave-end="transform opacity-0 scale-95"
-                class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg"
-              >
-                <div class="py-1 rounded-md bg-white shadow-xs">
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-                  >Your Profile</a>
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-                  >Settings</a>
-                  <a
-                    href="#"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
-                  >Sign out</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <searchbar :sidebarOpen="sidebarOpen" @toggleSidebar="toggleSidebar" />
       <main
         class="flex-1 relative z-0 overflow-y-auto py-6 focus:outline-none"
         tabindex="0"
@@ -208,14 +27,20 @@
 
 <script>
 import Sidebar from "@/components/Sidebar.vue";
+import Searchbar from "@/components/Searchbar.vue";
 
 export default {
-  components: { Sidebar },
+  components: { Sidebar, Searchbar },
   data() {
     return {
       sidebarOpen: false,
       open: false
     };
+  },
+  methods: {
+    toggleSidebar(toggleVal) {
+      sidebarOpen = toggleVal;
+    }
   }
 };
 </script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="h-screen flex overflow-hidden bg-gray-100" x-data="{ sidebarOpen: false }">
+    <!-- Off-canvas menu for mobile -->
+    <div class="md:hidden">
+      <div
+        @click="toggleSidebar()"
+        class="fixed inset-0 z-30 bg-gray-600 opacity-0 pointer-events-none transition-opacity ease-linear duration-300"
+        :class="{'opacity-75 pointer-events-auto': sidebarOpen, 'opacity-0 pointer-events-none': !sidebarOpen}"
+      ></div>
+
+      <!-- 
+        this was on class, but I needed to take it off for it to work
+      -translate-x-full-->
+      <div
+        class="fixed inset-y-0 left-0 flex flex-col z-40 max-w-xs w-full pt-5 pb-4 bg-red-800 transform ease-in-out duration-300"
+        :class="{'translate-x-0': sidebarOpen, '-translate-x-full': !sidebarOpen}"
+      >
+        <div class="absolute top-0 right-0 -mr-14 p-1">
+          <button
+            v-show="sidebarOpen"
+            @click="toggleSidebar()"
+            class="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
+          >
+            <svg class="h-6 w-6 text-white" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="flex-shrink-0 flex items-center px-4">
+          <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-brand.svg" alt="Workflow" />
+        </div>
+        <div class="mt-5 flex-1 h-0 overflow-y-auto">
+          <nav class="px-2">
+            <a
+              href="#"
+              class="group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-md text-white bg-red-900 focus:outline-none focus:bg-red-700 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-4 h-6 w-6 text-red-400 group-hover:text-red-300 group-focus:text-red-300 transition ease-in-out duration-150"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
+                />
+              </svg>
+              Dashboard
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+
+    <!-- Static sidebar for desktop -->
+    <div class="hidden md:flex md:flex-shrink-0">
+      <div class="flex flex-col w-64 bg-red-800 pt-5 pb-4">
+        <div class="flex items-center flex-shrink-0 px-4">
+          <img class="h-8 w-auto" src="/img/logos/workflow-logo-on-brand.svg" alt="Workflow" />
+        </div>
+        <div class="mt-5 h-0 flex-1 flex flex-col overflow-y-auto">
+          <!-- Sidebar component, swap this element with another sidebar if you like -->
+          <nav class="flex-1 px-2 bg-red-800">
+            <a
+              href="#"
+              class="group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md bg-red-900 focus:outline-none focus:bg-red-700 transition ease-in-out duration-150"
+            >
+              <svg
+                class="mr-3 h-6 w-6 text-red-400 group-focus:text-red-300 transition ease-in-out duration-150"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
+                />
+              </svg>
+              Dashboard
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["sidebarOpen"],
+  data() {
+    return {
+      // sidebarOpen: false
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      sidebarOpen = !sidebarOpen;
+      this.$emit("toggleSidebar", sidebarOpen);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
