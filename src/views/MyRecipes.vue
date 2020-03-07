d388c9a323987870535ba37dbbc47ff4a50d5a9f
Paul <pdevito3@gmail.com>
2020-03-07 15:57:01 -0500

    base menu built

--- src/views/MyRecipes.vue
+++ src/views/MyRecipes.vue
@@ -70,17 +70,74 @@
                   </div>
                 </div>
               </div>
-              <button>
-                <svg class="w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-                    stroke-width="2"
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </button>
+              <popper
+                trigger="clickToToggle"
+                :options="{
+                  placement: 'right-start',
+                  modifiers: { offset: { offset: '10px,10px' } }
+                }"
+              >
+                <div class="py-1 rounded-md bg-white shadow">
+                  <button
+                    class="flex block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      class="w-6 h-6 icon-folder-add"
+                    >
+                      <path
+                        class="fill-current text-gray-300"
+                        d="M4 4h7l2 2h7a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2z"
+                      />
+                      <rect
+                        width="20"
+                        height="12"
+                        x="2"
+                        y="8"
+                        class="fill-current text-gray-800"
+                        rx="2"
+                      />
+                      <path
+                        class="fill-current text-gray-300"
+                        d="M13 13h2a1 1 0 0 1 0 2h-2v2a1 1 0 0 1-2 0v-2H9a1 1 0 0 1 0-2h2v-2a1 1 0 0 1 2 0v2z"
+                      />
+                    </svg>
+                    <span class="pl-2">Add to Meal Plan</span>
+                  </button>
+                  <button
+                    class="flex block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 transition ease-in-out duration-150"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      class="w-6 h-6 icon-shopping-cart"
+                    >
+                      <path
+                        class="fill-current text-gray-300"
+                        d="M7 4h14a1 1 0 0 1 .9 1.45l-4 8a1 1 0 0 1-.9.55H7a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1z"
+                      />
+                      <path
+                        class="fill-current text-gray-800"
+                        d="M17.73 19a2 2 0 1 1-3.46 0H8.73a2 2 0 1 1-3.42-.08A3 3 0 0 1 5 13.17V4H3a1 1 0 1 1 0-2h3a1 1 0 0 1 1 1v10h11a1 1 0 0 1 0 2H6a1 1 0 0 0 0 2h12a1 1 0 0 1 0 2h-.27z"
+                      />
+                    </svg>
+                    <span class="pl-2">Add to Shopping List</span>
+                  </button>
+                </div>
+
+                <button slot="reference">
+                  <svg class="w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                      d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                      stroke-width="2"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </button>
+              </popper>
             </div>
           </a>
         </li>
@@ -109,7 +166,13 @@
 </template>
 
 <script>
-export default {};
+import Popper from "vue-popperjs";
+import "vue-popperjs/dist/vue-popper.css";
+export default {
+  components: {
+    popper: Popper
+  }
+};
 </script>
 
 <style>
