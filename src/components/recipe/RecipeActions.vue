56b1c66b962337d89351d1b390be7952051618c2
Paul DeVito <pdevito3@gmail.com>
2020-07-03 22:02:52 -0400

    minor cleanup

--- src/components/recipe/RecipeActions.vue
+++ src/components/recipe/RecipeActions.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex space-x-1 sm:space-x-2 md:space-x-1 lg:space-x-2">
+  <div class="flex">
     <span class="shadow-sm rounded-md">
       <router-link
         to="/myrecipes"
@@ -11,7 +11,7 @@
       </router-link>
     </span>
 
-    <span v-if="pageState=='view'" class="shadow-sm rounded-md">
+    <span v-if="pageState=='view'" class="ml-1 sm:ml-2 md:ml-1 lg:ml-2 shadow-sm rounded-md">
       <button
         @click="editRecipe()"
         type="button"
@@ -22,32 +22,30 @@
       </button>
     </span>
 
-    <div class="flex items-center justify-center">
-      <span v-if="pageState=='edit'" class="shadow-sm rounded-l-md">
-        <button
-          @click="saveRecipe()"
-          type="button"
-          class="inline-flex items-center px-3 sm:px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-l-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
-        >
-          <Save class ="h-5 w-5 fill-current" />
-          <p v-if="this.saving" class="hidden sm:block sm:pl-2 md:hidden lg:block">Saving...</p>
-          <p v-else class="hidden sm:block sm:pl-2 md:hidden lg:block">Save</p>
-        </button>
-      </span>
+    <span v-if="pageState=='edit'" class="ml-1 sm:ml-2 md:ml-1 lg:ml-2 shadow-sm rounded-l-md">
+      <button
+        @click="saveRecipe()"
+        type="button"
+        class="inline-flex items-center px-3 sm:px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-l-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
+      >
+        <Save class ="h-5 w-5 fill-current" />
+        <p v-if="this.saving" class="hidden sm:block sm:pl-2 md:hidden lg:block">Saving...</p>
+        <p v-else class="hidden sm:block sm:pl-2 md:hidden lg:block">Save</p>
+      </button>
+    </span>
 
-      <span v-if="pageState=='edit'" class="shadow-sm rounded-r-md">
-        <button
-          @click="cancelUpdate()"
-          type="button"
-          class="-ml-px inline-flex items-center px-3 sm:px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-r-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
-        >
-          <XCircle class="h-5 w-5 text-gray-500 fill-current" />
-          <p class="hidden sm:block sm:pl-2 md:hidden lg:block">Cancel</p>
-        </button>
-      </span>
-    </div>
+    <span v-if="pageState=='edit'" class="shadow-sm rounded-r-md">
+      <button
+        @click="cancelUpdate()"
+        type="button"
+        class="-ml-px inline-flex items-center px-3 sm:px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-r-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
+      >
+        <XCircle class="h-5 w-5 text-gray-500 fill-current" />
+        <p class="hidden sm:block sm:pl-2 md:hidden lg:block">Cancel</p>
+      </button>
+    </span>
 
-    <!-- <span class="shadow-sm rounded-md">
+    <!-- <span class="ml-1 sm:ml-2 md:ml-1 lg:ml-2 shadow-sm rounded-md">
       <button
         type="button"
         class="inline-flex items-center px-3 sm:px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:text-gray-800 active:bg-gray-50 active:text-gray-800 transition duration-150 ease-in-out"
@@ -58,7 +56,7 @@
       </button>
     </span> -->
 
-    <span class="shadow-sm rounded-md">
+    <span class="ml-1 sm:ml-2 md:ml-1 lg:ml-2 shadow-sm rounded-md">
       <button
         @click="openIngredientList()"
         type="button"
@@ -71,7 +69,7 @@
     </span>
 
     <span
-      class="hidden relative shadow-sm rounded-md"
+      class="hidden ml-1 sm:ml-2 md:ml-1 lg:ml-2 relative shadow-sm rounded-md"
     >
       <button
         @click="open = !open"
